38429e514b74a00ee22ea5cc746fe95f4d3d7997
ieltxu <ieltxu.lamikizca@elorrieta-errekamari.com>
2022-01-18 16:03:26 +0100

    ventanas de EN Favs y Top Ranking

--- euskalmet/src/main/java/vista/ENTopRanking.java
+++ euskalmet/src/main/java/vista/ENTopRanking.java
@@ -6,12 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JList;
-import javax.swing.JButton;
 
-public class EspaciosNaturalesFavoritos extends JFrame {
+public class ENTopRanking extends JFrame {
 
 	private JPanel contentPane;
 
@@ -22,7 +22,7 @@ public class EspaciosNaturalesFavoritos extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					EspaciosNaturalesFavoritos frame = new EspaciosNaturalesFavoritos();
+					ENTopRanking frame = new ENTopRanking();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,7 +34,7 @@ public class EspaciosNaturalesFavoritos extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public EspaciosNaturalesFavoritos() {
+	public ENTopRanking() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 500);
 		contentPane = new JPanel();
@@ -42,17 +42,17 @@ public class EspaciosNaturalesFavoritos extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblEspaciosNaturales = new JLabel("Espacios Naturales Favoritos");
-		lblEspaciosNaturales.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblEspaciosNaturales.setBounds(115, 33, 254, 53);
-		contentPane.add(lblEspaciosNaturales);
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(190, 379, 106, 33);
+		contentPane.add(btnVolver);
 		
 		JList list = new JList();
-		list.setBounds(115, 351, 254, -217);
+		list.setBounds(114, 343, 254, -217);
 		contentPane.add(list);
 		
-		JButton btnVolver = new JButton("Volver");
-		btnVolver.setBounds(183, 387, 106, 33);
-		contentPane.add(btnVolver);
+		JLabel lblTopEspaciosNaturales = new JLabel("Top Espacios Naturales");
+		lblTopEspaciosNaturales.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblTopEspaciosNaturales.setBounds(130, 24, 207, 53);
+		contentPane.add(lblTopEspaciosNaturales);
 	}
 }
